af906382db3128352ca828bd0017039afe59e1d7
freewind <nowindlee@gmail.com>
2019-01-08 14:24:44 +0800

    demo

--- hello.ts
+++ hello.ts
@@ -1,10 +1,14 @@
 import * as Koa from 'koa';
+import * as Router from 'koa-router';
 
 const app = new Koa();
 
-app.use(ctx => {
-    ctx.body = 'Hello, Koa';
+const router = new Router();
+router.get('/hello', ctx => {
+    ctx.body = 'Hello!'
 });
+
+app.use(router.routes());
 
 app.listen(3000);
 
